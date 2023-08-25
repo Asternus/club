665ef0a497efd373ba06e41225ef203883ba75a0
Asternus <malafiko@gmail.com>
2023-08-26 00:05:49 +0300

    validation

--- src/main/java/com/club/controllers/UserController.java
+++ src/main/java/com/club/controllers/UserController.java
@@ -3,18 +3,17 @@ package com.club.controllers;
 import com.club.entities.User;
 import com.club.services.UserService;
 import jakarta.validation.Valid;
+import jakarta.validation.ValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -41,7 +40,13 @@ public class UserController {
             return ResponseEntity.badRequest().body(errorResponse);
         }
 
-        userService.saveUser(user);
+        try {
+            userService.saveUser(user);
+        } catch (ValidationException e) {
+            final HashMap<String, String> emailNotUniq = new HashMap<>();
+            emailNotUniq.put("email", "Email already in use");
+            return ResponseEntity.badRequest().body(emailNotUniq);
+        }
 
         return ResponseEntity.ok(new HashMap<>());
     }
--- src/main/java/com/club/services/UserService.java
+++ src/main/java/com/club/services/UserService.java
@@ -3,7 +3,7 @@ package com.club.services;
 import com.club.entities.Role;
 import com.club.entities.User;
 import com.club.repositories.UserRepository;
-import jakarta.validation.Valid;
+import jakarta.validation.ValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -11,6 +11,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
 import java.util.Set;
 
 @Service
@@ -41,7 +42,13 @@ public class UserService implements UserDetailsService {
         return userRepository.findByEmail(email);
     }
 
-    public void saveUser(@Valid final User user) {
+    public void saveUser(final User user) {
+        final User userByEmail = userRepository.findByEmail(user.getEmail());
+
+        if (Objects.nonNull(userByEmail)) {
+            throw new ValidationException();
+        }
+
         user.setRoles(Set.of(Role.USER));
         final String encode = passwordEncoder.encode(user.getPassword());
         user.setPassword(encode);
